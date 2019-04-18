f9f46dc939d9c61d6a1bd5198904962a1fab28a3
NagelICT <info@nagelict.nl>
2019-04-18 20:15:32 +0200

    Added a first class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ public class Main {
     public static void main (String[] args)
     {
         showMessage("This is a shorter version of system.out.println");
+        Product product = new Product("Fictief",1);
+        Product product2 = new Product("Fictief 2",2);
+        Product product2_2 = new Product("Fictief 2",2);
+
+        System.out.println(product);
+        System.out.println(product.equals(product2));
+        System.out.println(product2.equals(product2_2));
     }
 
     private static void showMessage(String string) {
